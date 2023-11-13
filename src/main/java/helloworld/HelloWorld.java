3fc0ee1f0d5b2f634defda8550452e2f48f4127e
vivek <vivek.bk@deltax.com>
2023-11-13 22:27:08 +0530

    First Commit of hello world program

---
+++ src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class HelloWorld {
+
+  public static void main(final String[] args) {
+    System.out.println("Hello, World! This is the most basic Java program");
+  }
+
+}
