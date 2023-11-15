91ae213ebf3d606c4be021e665acf110291f54e2
vivek <vivek.bk@deltax.com>
2023-11-15 22:54:32 +0530

    Update message

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
   public static void main(final String[] args) {
-    System.out.println("Hello, World! This is Vivek with the most basic Java program");
+    System.out.println("Hello, World! This is Vivek with the most basic Java program.");
   }
 
 }
